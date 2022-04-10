26f251c8be5364d6de68b24b1afe5d064c5cc33d
shiomi680 <genki680@gmai.com>
2022-04-10 10:57:50 +0900

    sushiのbomサンプル追加

--- lib/sample_data/sushi.dart
+++ lib/sample_data/sushi.dart
@@ -1,6 +1,7 @@
 import '../model/material_info.dart';
 import '../model/parts_info.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../model/bom_info.dart';
 
 const SUSHI = MaterialInfo("maguro nigiri", "stdmenu-1",
     "This is most important nigiri in usual sushi restrant.",
@@ -8,6 +9,14 @@ const SUSHI = MaterialInfo("maguro nigiri", "stdmenu-1",
 
 const MAGURO = MaterialInfo("maguro", "maguro-1", "large fish",
     imagePath: "assets/parts/maguro/icon.png");
+
+const SHARI = MaterialInfo("shari", "rice-1", "Koshihikari from Nigata",
+    imagePath: "assets/parts/shari/icon.jpg");
+
+const SUSHI_INFO =
+    BomInfo(SUSHI, partsList: [PartsInfo(MAGURO, 1), PartsInfo(SHARI, 1)]);
+
+// final sushiBomProvider = StateNotifierProvider<
 
 final sushiPartsProvider = StateNotifierProvider<PartsInfoNotifier, PartsInfo>(
     (ref) => PartsInfoNotifier(PartsInfo(SUSHI, 1)));
